dd259803df5840c4e2c1d6640b18c18813298031
Judy Yu <judy.yuqiuyi@gmail.com>
2022-11-02 00:41:15 -0400

    Delete filename defined to write data to

--- BatteryLevel/Shared/SharedConstants.swift
+++ BatteryLevel/Shared/SharedConstants.swift
@@ -7,6 +7,3 @@ import Foundation
 
 // Identifier used for publishing notifications
 let myNotificationsIdentifier = "ca.on.lcs.BatteryLevel"
-
-// Define filename that we will write data to in the directory
-let savedBatteryLevelRemindersLabel = "savedBatteryLevelReminders"
